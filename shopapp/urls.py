76016dea757c866cc4b0ca02c8c8df6642b332e8
Magzhan Turmagambetov <magzhan2002kz@gmail.com>
2026-09-03 22:40:31 +0500

    added 20_module homework

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -6,7 +6,20 @@ from .views import (
     OrdersExportView, LatestProductsFeed,
     UserOrdersListView, UserOrdersExportView,  # новые классы
 )
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductArchiveView,
+    OrderListView, OrderDetailView, OrderCreateView, OrderUpdateView, OrderDeleteView,
+    OrdersExportView, LatestProductsFeed,
+    UserOrdersListView, UserOrdersExportView,
+)
 from .api import ProductViewSet, OrderViewSet
+
 
 app_name = 'shopapp'
 
@@ -15,6 +28,9 @@ router.register(r'products', ProductViewSet)
 router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # добавляем админку
+    path('', include(router.urls)),   # API через DRF
+
     # продукты
     path('products/', ProductListView.as_view(), name='products_list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
@@ -34,7 +50,7 @@ urlpatterns = [
     # новые маршруты для кеширования
     path("users/<int:user_id>/orders/", UserOrdersListView.as_view(), name="user_orders"),
     path("users/<int:user_id>/orders/export/", UserOrdersExportView.as_view(), name="user_orders_export"),
-
-    # API
-    path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
