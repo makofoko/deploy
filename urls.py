70573737e0af7abdfd8db549e55bba81dbd1c036
Magzhan Turmagambetov <magzhan2002kz@gmail.com>
2026-09-03 22:34:44 +0500

    added 20_module homework

--- urls.py
+++ urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),       # админка
-    path('', include('shopapp.urls')),     # твои маршруты из shopapp
+    path('admin/', admin.site.urls),
+    path('', include('shopapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
